c5ad9a36c5add359bf39aad182f3bf40fc2b2edb
Jake Durham <Jake Durham>
2022-09-07 01:10:48 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!, I'm Jake Durham\n");
+  printf("My major is Electrical Engineering!\n");
+
+  return 0;
+}
